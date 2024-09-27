f045dd382009b8070388b77ffe99470800944fd4
Kenny <kennykline12@gmail.com>
2024-09-27 11:17:42 -0700

    dont track  the db file directly, add a proof of concept for accessing the db

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,46 +1,26 @@
 import type { MetaFunction } from "@remix-run/node";
+import { useLoaderData } from "@remix-run/react";
 import db from "~/db.server";
 
 export const meta: MetaFunction = () => {
-  return [
-    { title: "Barter DB" },
-  ];
+  return [{ title: "Barter DB" }];
 };
 
-export function loader() {
-  const data = db.select("*").from("knex_migrations");
+export async function loader() {
+  const data = await db.select("*").from("users");
   return data;
 }
 
 export default function Index() {
+  const data = useLoaderData<typeof loader>();
 
   return (
-    <div className="flex h-screen items-center justify-center">
-      <div className="flex flex-col items-center gap-16">
-        <header className="flex flex-col items-center gap-9">
-          <h1 className="leading text-2xl font-bold text-gray-800 dark:text-gray-100">
-            Welcome to <span className="sr-only">Remix</span>
-          </h1>
-          <div className="h-[144px] w-[434px]">
-            <img
-              src="/logo-light.png"
-              alt="Remix"
-              className="block w-full dark:hidden"
-            />
-            <img
-              src="/logo-dark.png"
-              alt="Remix"
-              className="hidden w-full dark:block"
-            />
-          </div>
-        </header>
-        <nav className="flex flex-col items-center justify-center gap-4 rounded-3xl border border-gray-200 p-6 dark:border-gray-700">
-          <p className="leading-6 text-gray-700 dark:text-gray-200">
-            What&apos;s next?
-          </p>
-          <ul>
-          </ul>
-        </nav>
+    <div className="flex flex-col h-screen w-screen items-center justify-center">
+      <h1>Current Users</h1>
+      <div className="flex flex-col">
+        {data.map((user) => (
+          <p key={user.userId}>{user.username}</p>
+        ))}
       </div>
     </div>
   );
